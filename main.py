42f7331cf1bb59664aedb16192aeb9e798c98dd0
OmGoyal27 <omgoyal2711@gmail.com>
2024-05-28 20:12:46 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from pytube import YouTube
+import pyautogui as pg
 
-link = input("Enter the youtube link: ")
+link = pg.prompt(title="Youtube video downloader", text="Enter the youtube link.")      # Takes a vaue to get the youtube link.
 yt = YouTube(link)
 
-print("Title: ", yt.title)
-print("Views: ", yt.views)
+print("Title: ", yt.title)                          # Gets the title of the video
+print("Views: ", yt.views)                          # Gets the no. of views of the video.
 
 ys = yt.streams.get_audio_only()
-
 yd = yt.streams.get_highest_resolution()
+
+print(ys)
 
 ans = input("Audio or video(a/v): ")
 if ans == "a":
-    ys.download()
+    ys.download()                                   # Downloads an audio (But you might have to rename the file extention to .mp3)
 if ans == "v":
-    yd.download()
+    yd.download()                                   # Downloads a video
